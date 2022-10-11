9db49d3079569ba4275e3f113d9f420deda38910
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 01:45:58 -0700

    added router.put section

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,7 +60,24 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-});
+  Tag.update(req.body,
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+      .then((dbTagData) => {
+        if (!dbTagData) {
+          res.status(404).json({ message: "No tags found with this id."});
+          return;
+        }
+        res.json(dbTagData);
+      })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
