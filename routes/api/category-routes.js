a56cf2ba0931ff16e88d416caa67b1ad905e7dc7
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 02:56:01 -0700

    added router.delete section

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -82,6 +82,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+      Category.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCategoryData => {
+          if (!dbCategoryData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+          }
+          res.json(dbCategoryData);          
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
