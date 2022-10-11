c56c3c8aafce2585c63f4e54fc739e101ebd8f09
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 02:44:28 -0700

    added router.post section

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,16 +5,53 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
-});
+      Category.findAll({
+        include: [
+          {
+            model: Product,
+          }]
+      })
+        .then(dbCategoryData => res.json(dbCategoryData))
+        .catch(err => {
+          console.log(err);
+        });
+    });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+      Category.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [
+          {
+            model: Product,
+          }
+        ]
+      })
+      .then(dbCategoryData => {
+        if (!dbCategoryData) {
+          res.status(404).json({ message: 'No category found with this id'});
+          return;
+        }
+        res.json(dbCategoryData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      })
+    });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+      .then(dbCategoryData => res.json(dbCategoryData))
+      .catch(err => {
+        console.log(err);
+      });
 });
 
 router.put('/:id', (req, res) => {
