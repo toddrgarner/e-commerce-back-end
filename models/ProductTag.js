9bac40de98e52fb29200cfe57825748248033f10
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 03:27:48 -0700

    updated ProductTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,10 +4,8 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-    id: {
+ProductTag.init({
+     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
@@ -19,7 +17,7 @@ ProductTag.init(
         model: 'Product',
         key: 'id',
         unique: false
-      }
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
@@ -27,16 +25,14 @@ ProductTag.init(
         model: 'Tag',
         key: 'id',
         unique: false
-      }
-    }
-  },
-  {
+      },
+    },
+  },  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+  });
 
 module.exports = ProductTag;
