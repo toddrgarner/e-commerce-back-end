8e0425b6719c5880e9fe306087183bb5f777a745
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-11 03:24:41 -0700

    updated Product.js

--- models/Product.js
+++ models/Product.js
@@ -7,14 +7,13 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
+Product.init({
     // define columns
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
@@ -25,31 +24,29 @@ Product.init(
       allowNull: false,
       validate: {
         isDecimal: true
-      }
+        },
      },
      stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
         isNumeric: true
-      }
+        }
      },
      category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
-      }
-     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        },
+      },
+ }, {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product',
+  });
 
 module.exports = Product;
